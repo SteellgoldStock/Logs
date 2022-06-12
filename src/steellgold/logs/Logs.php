a0d9aeb62a931c206264ec0ed6fdd13701ff107c
Gaëtan H <steellgold0@gmail.com>
2022-06-12 18:36:55 +0200

    add base

--- src/steellgold/logs/Logs.php
+++ src/steellgold/logs/Logs.php
@@ -2,6 +2,21 @@
 
 namespace steellgold\logs;
 
-class Logs {
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\plugin\PluginBase;
 
+class Logs extends PluginBase implements Listener {
+	public Logs $instance;
+
+	public function onEnable() : void {
+		$this->instance = $this;
+		if(!file_exists($this->getDataFolder() . "config.yml")) {
+			$this->saveResource("config.yml",true);
+		}
+	}
+
+	public function onDrop(PlayerDropItemEvent $event){
+		$event = $event->getPlayer();
+	}
 }
